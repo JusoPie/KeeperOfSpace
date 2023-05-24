6632acb80f287831ba560d5e15e186bf9874a33b
JusoPie <128028155+JusoPie@users.noreply.github.com>
2023-05-24 16:00:02 +0300

    24.5.2023
    blink effect

--- Assets/Scripts/Player/Blink.cs
+++ Assets/Scripts/Player/Blink.cs
@@ -6,6 +6,12 @@ public class Blink : MonoBehaviour
 {
     public float blinkDistance = 2.0f;
 
+    public int blinks = 3;
+
+    public float blinktimer = 5f;
+
+    public GameObject[] blinkImages;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,32 +21,75 @@ public class Blink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse1))
+        if (blinks == 3)
         {
-            Vector3 blinkDirection = Vector3.zero;
+            blinkImages[2].SetActive(true);
+            blinkImages[1].SetActive(true);
+            blinkImages[0].SetActive(true);
+        }
+        if (blinks == 2)
+        {
+            blinkImages[2].SetActive(false);
+            blinkImages[1].SetActive(true);
+            blinkImages[0].SetActive(true);
+        }
+        if (blinks == 1)
+        {
+            blinkImages[2].SetActive(false);
+            blinkImages[1].SetActive(false);
+            blinkImages[0].SetActive(true);
+        }
+        if (blinks == 0)
+        {
+            blinkImages[2].SetActive(false);
+            blinkImages[1].SetActive(false);
+            blinkImages[0].SetActive(false);
+        }
+        if (blinks != 3) 
+        {
+            blinktimer -= Time.deltaTime;
+            if (blinktimer <= 0) 
+            {
+                blinks++;
+                blinktimer = 5f;
+            }
+        }
 
-            if (Input.GetKeyDown(KeyCode.W))
+        if (blinks > 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
             {
-                blinkDirection += Vector3.up;
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                blinkDirection += Vector3.down;
-            }
+                Vector3 blinkDirection = Vector3.zero;
 
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                blinkDirection += Vector3.left;
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                blinkDirection += Vector3.right;
-            }
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    blinkDirection += Vector3.up;
+                    blinks--;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    blinkDirection += Vector3.down;
+                    blinks--;
+                }
 
-            if (blinkDirection != Vector3.zero)
-            {
-                Vector3 blinkDestination = transform.position + blinkDirection.normalized * blinkDistance;
-                transform.position = blinkDestination;
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    blinkDirection += Vector3.left;
+                    blinks--;
+                }
+                else if (Input.GetKeyDown(KeyCode.D))
+                {
+                    blinkDirection += Vector3.right;
+                    blinks--;
+                }
+
+                if (blinkDirection != Vector3.zero)
+                {
+                    Vector3 blinkDestination = transform.position + blinkDirection.normalized * blinkDistance;
+                    transform.position = blinkDestination;
+
+                }
+
             }
         }
     }
