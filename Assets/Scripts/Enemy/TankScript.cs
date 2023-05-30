c6b212d0de334829609aab04a58400f6ccd43796
JusoPie <128028155+JusoPie@users.noreply.github.com>
2023-05-30 14:56:13 +0300

    30.5.2023
    fixed build problem. tank introduction. started killCounters

--- Assets/Scripts/Enemy/TankScript.cs
+++ Assets/Scripts/Enemy/TankScript.cs
@@ -9,9 +9,15 @@ public class TankScript : MonoBehaviour
     public float enemySpeed = 1f;
     public float radius = 5f;
 
+    [SerializeField]
+    private float waitTime = 3f;
+
+    public bool canMove = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("StartOrbiting");
         target = GameObject.FindGameObjectWithTag("Player").transform;
         targetObject = GameObject.FindGameObjectWithTag("TankTarget").transform;
     }
@@ -19,14 +25,26 @@ public class TankScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        if (canMove == false)
+        {
+            transform.Translate(transform.up * -enemySpeed * Time.deltaTime);
+            transform.LookAt(targetObject);
+        }
 
-        Vector3 difference = target.position - transform.position;
-        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ - 90f);
 
-        transform.Rotate(-90f, 0f, 0f, Space.Self);
-        transform.RotateAround(targetObject.transform.position, new Vector3(0, 0, 10),enemySpeed * Time.deltaTime);
+
+        if (canMove == true)
+        {
+            //enemySpeed = 3f;
+            transform.LookAt(target);
+
+            Vector3 difference = target.position - transform.position;
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ - 90f);
+
+            transform.Rotate(-90f, 0f, 0f, Space.Self);
+            transform.RotateAround(targetObject.transform.position, new Vector3(0, 0, 10), enemySpeed * Time.deltaTime);
+        }
         // Calculate the desired position in a circle around the target point
         /*float angle = Time.time * enemySpeed;
         Vector3 desiredPosition = targetObject.position + new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * radius;
@@ -37,5 +55,13 @@ public class TankScript : MonoBehaviour
 
 
 
+    }
+
+    private IEnumerator StartOrbiting() 
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        canMove = true;
+    
     }
 }
